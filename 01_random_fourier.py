d32c198dddb0c93ee242cdc3d5c94648678fa099
12qw1q2w <44050246+12qw1q2w@users.noreply.github.com>
2018-10-13 03:21:44 +0900

    Update 01_random_fourier.py

--- 01_random_fourier.py
+++ 01_random_fourier.py
@@ -9,7 +9,7 @@ def tensor_sum(f, n0, N):
         s += f(n)
     return s
 
-def random_fourier(x, nmax=np.random.randint(0, 32)):
+def random_fourier(x, nmax):
     a = np.random.random(nmax+1)-0.5
     b = np.random.random(nmax+1)-0.5
     f = lambda x: tensor_sum(lambda n: a[n]*np.sin(n*np.pi*x) + b[n]*np.cos(n*np.pi*x), 0, nmax)
@@ -18,5 +18,5 @@ def random_fourier(x, nmax=np.random.randint(0, 32)):
 x = np.linspace(0, 1, N+1)
 
 while True:
-    plt.plot(x, random_fourier(x))
+    plt.plot(x, random_fourier(x, np.random.randint(0, 32)))
     plt.show()
