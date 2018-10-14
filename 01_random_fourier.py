4f2bd10bf850fea3c874d0ee3e51c0dd057cbc16
Lee, Ji-ho <44050246+12qw1q2w@users.noreply.github.com>
2018-10-14 13:17:15 +0900

    Update 01_random_fourier.py

--- 01_random_fourier.py
+++ 01_random_fourier.py
@@ -4,16 +4,15 @@ import matplotlib.pyplot as plt
 N = 100
 
 def tensor_sum(f, n0, N):
-    s = np.zeros(f(n0).shape)
-    for n in range(n0, N+1):
+    s = f(n0)
+    for n in range(n0+1, N+1):
         s += f(n)
     return s
 
 def random_fourier(x, nmax):
     a = np.random.random(nmax+1)-0.5
     b = np.random.random(nmax+1)-0.5
-    f = lambda x: tensor_sum(lambda n: a[n]*np.sin(n*np.pi*x) + b[n]*np.cos(n*np.pi*x), 0, nmax)
-    return f(x)
+    return tensor_sum(lambda n: a[n]*np.sin(n*np.pi*x) + b[n]*np.cos(n*np.pi*x), 0, nmax)
 
 x = np.linspace(0, 1, N+1)
 
